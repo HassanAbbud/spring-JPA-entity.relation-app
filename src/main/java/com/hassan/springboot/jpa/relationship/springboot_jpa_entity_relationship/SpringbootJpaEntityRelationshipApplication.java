baa25465630ef30b0b4136d46a4f8d93d8f6416e
Hassan Abbud <hassanabbud@hotmail.com>
2024-08-15 13:00:33 -0600

    first commit

---
+++ src/main/java/com/hassan/springboot/jpa/relationship/springboot_jpa_entity_relationship/SpringbootJpaEntityRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.hassan.springboot.jpa.relationship.springboot_jpa_entity_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaEntityRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaEntityRelationshipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hassan/springboot/jpa/relationship/springboot_jpa_entity_relationship/SpringbootJpaEntityRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hassan.springboot.jpa.relationship.springboot_jpa_entity_relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaEntityRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
